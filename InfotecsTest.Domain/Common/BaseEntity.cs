06f46fe885d5f164f9982564aff93dc1fe3b279c
Vladimir <117316977+LySpacy@users.noreply.github.com>
2026-07-24 04:32:10 +0300

    Update BaseEntity.cs

--- InfotecsTest.Domain/Common/BaseEntity.cs
+++ InfotecsTest.Domain/Common/BaseEntity.cs
@@ -1,6 +1,9 @@
 ﻿namespace InfotecsTest.Domain.Common;
 
-public class BaseEntity
+/// <summary>
+/// Базовая модель сущности, хранящейся в базе данных.
+/// </summary>
+public abstract class BaseEntity
 {
     public Guid Id { get; init; } = Guid.NewGuid();
 }
